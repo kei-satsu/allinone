b22123855f28e357cd538d46f793f71759873d8a
kei-satsu <yephoneaungmdl@gmail.com>
2026-08-16 16:45:30 +0630

    Add options for 500 and 1000 in select dropdown

--- app/senders/page.tsx
+++ app/senders/page.tsx
@@ -1319,6 +1319,8 @@ const totalSelectedCod = getDisplayOrders()
                               <option value={50}>50</option>
                               <option value={100}>100</option>
                               <option value={200}>200</option>
+                              <option value={500}>500</option>
+                              <option value={1000}>1000</option>
                             </select>
                             <span>
                               Showing {indexOfFirstItem + 1} -{" "}
